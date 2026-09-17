00e40a8bc3ca0794361764977048a1eb59c4ffda
berinsteindiego-collab <berinstein.diego@gmail.com>
2026-09-17 15:42:09 -0300

    Update page.tsx

--- src/app/proyectos/page.tsx
+++ src/app/proyectos/page.tsx
@@ -1,1 +1,11 @@
+import AskBox from "@/components/AskBox";
+import Panorama from "@/components/Panorama";
 
+export default function ProyectosPage() {
+  return (
+    <>
+      <Panorama />
+      <AskBox />
+    </>
+  );
+}
